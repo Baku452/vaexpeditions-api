8b7f74ba4668fac7dbfd7f586fd08d01a8951902
anaidjm1 <anaidjm1@gmail.com>
2021-04-05 14:48:56 -0500

    email sending

--- specialists/views.py
+++ specialists/views.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from rest_framework.views import APIView
 from django.http import Http404
 from rest_framework.response import Response
@@ -10,11 +13,11 @@ from django.core.mail import send_mail
 # from .models import ContactUs, Newsletter
 from .serializers import ContactUsSerializer, NewsletterSerializer
 
-subject = ' '
+subject = 'Web Opportunity '
 html_message = render_to_string('mail_template.html', {'context': 'values'})
 plain_message = strip_tags(html_message)
-from_email = 'From <michel.hinostroza@gmail.com>'
-to = 'michel.hinostroza@gmail.com'
+from_email = os.getenv("DJANGO_FROM_MAIL")
+to = os.getenv("DJANGO_TO_MAIL")
 
 
 class ContactCreateApi(APIView):
@@ -22,7 +25,7 @@ class ContactCreateApi(APIView):
         serializer = ContactUsSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+            send_mail(subject+serializer.data['package'], plain_message,serializer.data['first_name']+ ' '+serializer.data['last_name']+from_email, [to], html_message=render_to_string('contactTemplate.html', serializer.data))
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
