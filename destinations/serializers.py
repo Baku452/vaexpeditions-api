acc1a0c3c679b8794460ede72fed8bbe1d81f3ff
Baku452 <aiacos12@outlook.com>
2021-12-01 11:59:42 -0500

    menu Destinations serializer

--- destinations/serializers.py
+++ destinations/serializers.py
@@ -1,14 +1,25 @@
-from .models import Country, Banner, Destination, Reason, WeatherItems, FaqDest, DestinationImage, ContinentImage, WhereToGo, ItemWhere, TravelAdvice
+from .models import (
+    Country,
+    Banner,
+    Destination,
+    Reason,
+    WeatherItems,
+    FaqDest,
+    DestinationImage,
+    ContinentImage,
+    WhereToGo,
+    ItemWhere,
+    TravelAdvice,
+)
 from packages.models import Package
 from rest_framework import serializers
 from packages.serializers import PackageTypeSerializer, PackageSerializer
 
 
 class WeatherItemsSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = WeatherItems
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ReasonSerializer(serializers.ModelSerializer):
@@ -18,47 +29,81 @@ class ReasonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Reason
-        fields = '__all__'
+        fields = "__all__"
+
+
+class ReasonHomeSerializer(serializers.ModelSerializer):
+
+    thumbnail = serializers.ImageField(read_only=True)
+    original = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Reason
+        fields = "__all__"
+
 
 class FAQsSerializer(serializers.ModelSerializer):
     class Meta:
         model = FaqDest
-        fields = '__all__'
+        fields = "__all__"
+
 
 class TravelAdviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = TravelAdvice
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemWhere
-        fields = '__all__'
+        fields = "__all__"
+
 
 class WhereSerializer(serializers.ModelSerializer):
     thumbnail = serializers.ImageField(read_only=True)
     items = ItemsSerializer(many=True, read_only=True)
+
     class Meta:
         model = WhereToGo
-        fields = '__all__'
+        fields = "__all__"
+
+
+class WhereHomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WhereToGo
+        fields = ["id", "title"]
+
 
 class DestImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = DestinationImage
-        fields = '__all__' 
+        fields = "__all__"
+
 
 class PackageSerializer(serializers.ModelSerializer):
     thumbnail = serializers.ImageField(read_only=True)
     type_name = serializers.StringRelatedField(many=True, source="package_type")
     activity_name = serializers.StringRelatedField(source="activity")
+
     class Meta:
         model = Package
-        fields = ['id','title','days','slug','type_name','summary','thumbnail','activity_name']
+        fields = [
+            "id",
+            "title",
+            "days",
+            "slug",
+            "type_name",
+            "summary",
+            "thumbnail",
+            "activity_name",
+        ]
+
 
 class ContinentImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = DestinationImage
-        fields = '__all__' 
+        fields = "__all__"
 
 
 class DestinationSerializer(serializers.ModelSerializer):
@@ -75,14 +120,25 @@ class DestinationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Destination
-        fields = '__all__'
+        fields = "__all__"
+
 
 class DestinationHomeSerializer(serializers.ModelSerializer):
+    where = WhereHomeSerializer(many=True, read_only=True)
+    thumbnail = serializers.ImageField(read_only=True)
 
     class Meta:
         model = Destination
-        fields = ['id','title','sub_title','image','slug']
-
+        fields = [
+            "id",
+            "title",
+            "summary",
+            "sub_title",
+            "image",
+            "thumbnail",
+            "slug",
+            "where",
+        ]
 
 
 class CountrySerializer(serializers.ModelSerializer):
@@ -95,23 +151,28 @@ class CountrySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Country
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CountryHomeSerializer(serializers.ModelSerializer):
     destinations = DestinationHomeSerializer(many=True, read_only=True)
+
     class Meta:
         model = Country
-        fields = ['id','name','slug','destinations']
+        fields = ["id", "name", "slug", "destinations"]
+
 
 class BannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Banner
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CitySerializer(serializers.ModelSerializer):
     thumbnail = serializers.ImageField(read_only=True)
     items = ItemsSerializer(many=True, read_only=True)
-    slug_destination = serializers.CharField(source='Destination.slug')
+    slug_destination = serializers.CharField(source="Destination.slug")
+
     class Meta:
         model = WhereToGo
-        fields = '__all__'
+        fields = "__all__"
